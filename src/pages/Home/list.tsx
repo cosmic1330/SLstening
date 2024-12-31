e7de1a18f35d25a38663b2466875b49ef88dc8a4
楊竣宇 <yangjunyu@yangjunyudeMacBook-Air.local>
2025-01-01 00:13:50 +0800

    feat: add tauri http & swr

--- src/pages/Home/list.tsx
+++ src/pages/Home/list.tsx
@@ -1,8 +1,14 @@
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router";
+import useSWR from 'swr';
+import { tauriFetcher } from "../../api/http";
+
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 function List() {
   let navigate = useNavigate();
+  const { data, error, isLoading } = useSWR(API_URL, tauriFetcher);
+  console.log(data, error, isLoading);
   return (
     <div>
       <Button

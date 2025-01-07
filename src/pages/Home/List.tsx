8d64ae4d4a5e46a1bf90b21dd8a117bf5ca55df6
KimYang <kimm.jy.yang@mail.foxconn.com>
2025-01-07 16:07:20 +0800

    feat: add no stock button

--- src/pages/Home/List.tsx
+++ src/pages/Home/List.tsx
@@ -1,8 +1,9 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { listen } from "@tauri-apps/api/event";
 import { useEffect } from "react";
 import StockBox from "../../components/StockBox";
 import useStocksStore from "../../store/Stock.store";
+import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
 
 function List() {
   const { stocks, reload, increase } = useStocksStore();
@@ -20,11 +21,28 @@ function List() {
     };
   }, [increase]);
 
+  const openAddWindow = async () => {
+    const webview = new WebviewWindow("add", {
+      title: "Add Stock Id",
+      url: "/add",
+      width: 300,
+      height: 130,
+    });
+    webview.once("tauri://created", function () {});
+    webview.once("tauri://error", function (e) {
+      console.log(e);
+    });
+  };
+
   return (
-    <Box>
-      {stocks.map((id) => (
-        <StockBox key={id} id={id} />
-      ))}
+    <Box my={2}>
+      {stocks.length === 0 ? (
+        <Button fullWidth variant="contained" onClick={openAddWindow}>
+          Add First Stock
+        </Button>
+      ) : (
+        stocks.map((id) => <StockBox key={id} id={id} />)
+      )}
     </Box>
   );
 }

1188c842f3826826887c6b7034d47fff18921db2
楊竣宇 <yangjunyu@yangjunyudeMacBook-Air.local>
2025-01-10 22:24:48 +0800

    bug: plugin_window_state in macOS, width & height not sync

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,7 +9,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())

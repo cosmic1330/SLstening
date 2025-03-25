17e68205b1e9ef957b322adfab1dc71759e6b3c1
KimYang <kimm.jy.yang@mail.foxconn.com>
2025-03-25 11:44:16 +0800

    fix: update timeout bug (error decoding response body)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 mod sqlite;
 
-use tauri_plugin_updater::UpdaterExt;
 use tauri_plugin_dialog::{DialogExt, MessageDialogButtons, MessageDialogKind};
+use tauri_plugin_updater::UpdaterExt;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -10,57 +10,68 @@ fn greet(name: &str) -> String {
 }
 
 async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
-  if let Some(update) = app.updater_builder()
-  .timeout(std::time::Duration::from_secs(30))
-  .build()?
-  .check()
-  .await? {
-    let version = &update.version;
+    if let Some(update) = app.updater()?.check().await? {
+        let version = &update.version;
 
-    let ans = app
-      .dialog()
-      .message(&format!("Update available: v{}", version))
-      .kind(MessageDialogKind::Info)
-      .buttons(MessageDialogButtons::OkCancel) 
-      .blocking_show();
+        let ans = app
+            .dialog()
+            .message(&format!("Update available: v{}", version))
+            .kind(MessageDialogKind::Info)
+            .buttons(MessageDialogButtons::OkCancel)
+            .blocking_show();
 
-    let mut downloaded = 0;
-    if ans {
-      update.download_and_install(|chunk_length, content_length| {
-            downloaded += chunk_length;
-            print!("下載中 ({} bytes)\r", downloaded);
+        let mut downloaded = 0;
+        if ans {
+            if let Err(e) = update
+                .download_and_install(
+                    |chunk_length, content_length| {
+                        downloaded += chunk_length;
 
-            // 修正進度顯示格式
-            let progress = match content_length {
-              Some(total) => format!("{:.2}%", (downloaded as f64 / total as f64) * 100.0),
-              None => format!("{} bytes", downloaded),
-            };
-            println!("{}", progress);
-          },
-          || {
-            println!("download finished");
-            // 下載完成後，顯示訊息框詢問是否重新啟動應用程式
-            let restart_ans = app
-              .dialog()
-              .message("Update downloaded. Restart now?")
-              .kind(MessageDialogKind::Info)
-              .buttons(MessageDialogButtons::OkCancel) 
-              .blocking_show();
+                        // 修正進度顯示格式
+                        let progress = match content_length {
+                            Some(total) => {
+                                format!(
+                                    "downloaded {:.2}%",
+                                    (downloaded as f64 / total as f64) * 100.0
+                                )
+                            }
+                            None => format!("{} bytes", downloaded),
+                        };
+                        println!("{}", progress);
+                    },
+                    || {
+                        println!("download finished");
+                        // 下載完成後，顯示訊息框詢問是否重新啟動應用程式
+                        let restart_ans = app
+                            .dialog()
+                            .message("Update downloaded. Restart now?")
+                            .kind(MessageDialogKind::Info)
+                            .buttons(MessageDialogButtons::OkCancel)
+                            .blocking_show();
 
-            if restart_ans {
-              app.restart();
+                        if restart_ans {
+                            app.restart();
+                        }
+                    },
+                )
+                .await
+            {
+                println!("Download Update failed message: {}", e);
+                app.dialog()
+                    .message(&format!("Download Update failed: {}", e))
+                    .kind(MessageDialogKind::Warning)
+                    .buttons(MessageDialogButtons::Ok)
+                    .blocking_show();
             }
-          }).await?;
+        } else {
+            println!("User canceled the update.");
+        }
     } else {
-      println!("User canceled the update.");
+        println!("No updates available.");
     }
-  } else {
-    println!("No updates available.");
-  }
 
-  Ok(())
+    Ok(())
 }
-
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -82,10 +93,10 @@ pub fn run() {
         .setup(|app| {
             let handle = app.handle().clone();
             tauri::async_runtime::spawn(async move {
-              update(handle).await.unwrap();
+                update(handle).await.unwrap();
             });
             Ok(())
-          })
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");

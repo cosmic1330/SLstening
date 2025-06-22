fc06b2a7db43c32ddd45095121ee8bf33b7263df
楊竣宇 <yangjunyu@yangjunyudeMacBook-Air.local>
2025-06-22 22:32:29 +0800

    fix: change supabase api key

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://zifjsqcvahkpytcklzed.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppZmpzcWN2YWhrcHl0Y2tsemVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3OTQwOTgsImV4cCI6MjA1ODM3MDA5OH0.6-TfHphUfK9nUMRj0RIA6ZaMSCFvsMBiH5HsM_Zjj48";
+  "sb_publishable_3zxE5vieYY_1OKCWAfPlAw_SSlSUqv8";
 export const supabase = createClient(supabaseUrl, supabaseKey);

622294340f0d4c5601d605c9fc661d5ea799f525
Kim <you@example.com>
2025-08-15 15:32:52 +0800

    remove: schoice

--- src/pages/Schoice/parts/Process.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function Process({ persent }: { persent: number }) {
-  return (
-    <Typography variant="body2" color="text.secondary">
-      {`下載中 ${persent}%`}
-    </Typography>
-  );
-}
